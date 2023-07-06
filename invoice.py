9e25d4de002263ecd50fe90185d6902c65339325
Juanjo Garcia <juanjo.garcia@nan-tic.com>
2023-07-06 15:59:30 +0200

    Add message if we dont have a reference in the invoice.
    Task #160297

---
+++ invoice.py
@@ -0,0 +1,29 @@
+# This file is part account_invoice_reference_required module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.pool import PoolMeta, Pool
+from trytond.i18n import gettext
+from trytond.exceptions import UserWarning
+
+
+class Invoice(metaclass=PoolMeta):
+    __name__ = 'account.invoice'
+
+    @classmethod
+    def set_number(cls, invoices):
+        pool = Pool()
+        Warning = pool.get('res.user.warning')
+
+        super().set_number(invoices)
+
+        for invoice in invoices:
+            if invoice.type != 'in':
+                continue
+            if not invoice.reference or invoice.reference == '':
+                warning_key = Warning.format('invoice_no_reference', [invoice])
+
+                if Warning.check(warning_key):
+                    raise UserWarning(warning_key, gettext(
+                        'account_invoice_reference_required.msg_no_reference_found',
+                        invoice=invoice.rec_name))
+
